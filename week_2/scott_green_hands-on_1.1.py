c289721d7ac78a798d9e506b98eea5e6b81e4db0
Scott Green <skevingreen@gmail.com>
2025-01-18 18:11:45 -0700

    completed assignment 2.2

---
+++ week_2/scott_green_hands-on_1.1.py
@@ -0,0 +1,9 @@
+""" 
+    Title: scott_green_hands-on_1.1.py
+    Author: Scott Green
+    Date: 18 January 2025
+    Description: Example file for outputting a quote.
+"""
+
+# Print the quote to the console
+print('"There is nothing permanent except change." - Heraclitus')
